dadb94503383f7edcb5dbcd5f8459f562441129f
Bartlomiej Cichy <148818732+VBlackened@users.noreply.github.com>
2026-07-04 17:49:11 +0200

    Prepared UI for mailing from Contacts and Categories Pages, fix for Contacts search responsivity, fixed bug for JS disposing

---
+++ CircleHub.Client/Models/EmailData.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CircleHub.Client.Models;
+
+public class EmailData
+{
+    public required string Recipients { get; set; }
+
+    [Length(5, 100, ErrorMessage = "Subject must be between 5 and 100 characters.")]
+    public required string Subject { get; set; }
+
+    [Length(10, 600, ErrorMessage = "Body must be between 10 and 600 characters.")]
+    public required string Body { get; set; }
+}
